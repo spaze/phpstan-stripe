252b818db4f18356adaffceae656912eb31ab6fd
Michal Špaček <mail@michalspacek.cz>
2018-10-31 22:18:45 +0100

    Emulated properties for save() requests

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # phpstan-stripe
 Stripe SDK extension for PHPStan
+
+PHP library for the Stripe API uses PHP objects where some of the properties are used only when the object is updated.
+
+- `Customer::$source`, [used only for `save()`](https://stripe.com/docs/api/customers/update#update_customer-source)
+- `Subscription::$coupon`, [used only for `save()`](https://stripe.com/docs/api/subscriptions/update#update_subscription-coupon)
+
+These are not documented using `@property` tags on the classes, and the dev team [feels](https://github.com/stripe/stripe-php/pull/543) it should stay this way. Honestly, I'm not sure adding `@property` tags would be the best way either.
+
+PHPStan will obviously flag such property access and this extension will resolve those errors by telling PHPStan such properties exist.
+
+## Installation
+Add this to your `phpstan.neon`:
+
+```
+includes:
+    - vendor/spaze/phpstan-stripe/extension.neon
+```
---
+++ src/RequestProperty.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types = 1);
+
+namespace Spaze\PHPStan\Reflection\Stripe;
+
+use PHPStan\Reflection\ClassReflection;
+use PHPStan\Reflection\Php\UniversalObjectCrateProperty;
+use PHPStan\Reflection\PropertiesClassReflectionExtension;
+use PHPStan\Reflection\PropertyReflection;
+use PHPStan\ShouldNotHappenException;
+use PHPStan\Type\ArrayType;
+use PHPStan\Type\BooleanType;
+use PHPStan\Type\FloatType;
+use PHPStan\Type\IntegerType;
+use PHPStan\Type\MixedType;
+use PHPStan\Type\StringType;
+use PHPStan\Type\Type;
+
+class RequestProperty implements PropertiesClassReflectionExtension
+{
+
+	/** @var string[][] */
+	private $properties = [];
+
+
+	public function __construct(array $properties)
+	{
+		foreach ($properties as $property => $type) {
+			list($className, $propertyName) = explode('::', $property);
+			$this->properties[$className][ltrim($propertyName, '$')] = $type;
+		}
+	}
+
+
+	public function hasProperty(ClassReflection $classReflection, string $propertyName): bool
+	{
+		return isset($this->properties[$classReflection->getName()][$propertyName]);
+	}
+
+
+	public function getProperty(ClassReflection $classReflection, string $propertyName): PropertyReflection
+	{
+		return new UniversalObjectCrateProperty($classReflection, $this->getTypeFromString($this->properties[$classReflection->getName()][$propertyName]));
+	}
+
+
+	private function getTypeFromString(string $typeString): Type
+	{
+		switch (strtolower($typeString)) {
+			case 'int':
+				return new IntegerType();
+			case 'bool':
+				return new BooleanType();
+			case 'string':
+				return new StringType();
+			case 'float':
+				return new FloatType();
+			case 'array':
+				return new ArrayType(new MixedType(), new MixedType());
+			default:
+				throw new ShouldNotHappenException("Unknown type {$typeString}");
+		}
+	}
+
+}
