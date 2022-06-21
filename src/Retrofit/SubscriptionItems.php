7c612647e7c9814c6c48acc8c195d4a010dc3e3b
Michal Špaček <mail@michalspacek.cz>
2022-06-21 06:30:01 +0200

    Add Collection type
    Resolve PHPStan's "Class Spaze\PHPStan\Stripe\Retrofit\SubscriptionItems extends generic class Stripe\Collection but does not specify its types: TStripeObject"

--- src/Retrofit/SubscriptionItems.php
+++ src/Retrofit/SubscriptionItems.php
@@ -4,9 +4,12 @@ declare(strict_types = 1);
 namespace Spaze\PHPStan\Stripe\Retrofit;
 
 use Stripe\Collection;
+use Stripe\StripeObject;
 use Stripe\SubscriptionItem;
 
 /**
+ * @template TStripeObject of StripeObject
+ * @template-extends Collection<TStripeObject>
  * @property array<int, SubscriptionItem> $data
  */
 class SubscriptionItems extends Collection
