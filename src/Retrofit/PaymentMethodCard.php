1454a786441a5286e24ee8f00ce917dc09fc7715
Fran Moreno <franmomu@gmail.com>
2022-11-04 10:00:39 +0100

    Add properties for PaymentMethod::$card
    see https://stripe.com/docs/api/payment_methods/object#payment_method_object-card

---
+++ src/Retrofit/PaymentMethodCard.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types = 1);
+
+namespace Spaze\PHPStan\Stripe\Retrofit;
+
+/**
+ * @property 'amex'|'diners'|'discover'|'jcb'|'mastercard'|'unionpay'|'visa'|'unknown' $brand
+ * @property StripeObject $checks
+ * @property string $country
+ * @property int $exp_month
+ * @property int $exp_year
+ * @property string $fingerprint
+ * @property string $funding
+ * @property StripeObject $generated_from
+ * @property StripeObject $three_d_secure_usage
+ * @property StripeObject $wallet
+ */
+class PaymentMethodCard
+{
+}
