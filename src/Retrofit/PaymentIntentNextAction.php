d66a08d47622af6e3bd8056bfdbd680509e50bdc
Fran Moreno <franmomu@gmail.com>
2022-11-04 10:23:22 +0100

    Add properties for PaymentIntent::next_action
    see https://stripe.com/docs/api/payment_intents/object#payment_intent_object-next_action

---
+++ src/Retrofit/PaymentIntentNextAction.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types = 1);
+
+namespace Spaze\PHPStan\Stripe\Retrofit;
+
+/**
+ * @property StripeObject $redirect_to_url
+ * @property 'redirect_to_url'|'use_stripe_sdk'|'alipay_handle_redirect'|'oxxo_display_details'|'verify_with_microdeposits' $type
+ */
+class PaymentIntentNextAction
+{
+}
